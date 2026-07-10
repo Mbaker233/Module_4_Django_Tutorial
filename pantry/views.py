e0799b128386f63b6706d957d6511dcc8fc6fcd6
Mathew Baker <mbaker233@ivytech.edu>
2026-07-10 10:14:29 -0400

    Add pantry homepage with views urls templates and ORM

--- pantry/views.py
+++ pantry/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import FoodItem
 
-# Create your views here.
+
+def food_list(request):
+    food_items = FoodItem.objects.all().order_by('expiration_date')
+    return render(request, 'pantry/food_list.html', {'food_items': food_items})
